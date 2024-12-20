c004209052a3d1f269c19fb26d0ffd14f2ec17cc
Jialong Wu <manchery@outlook.com>
2024-12-20 08:11:48 +0000

    add bair finetune scripts

---
+++ scripts/finetune/bair-64-act-free.sh
@@ -0,0 +1,21 @@
+accelerate launch train_tokenizer.py \
+    --exp_name bair_tokenizer_ft --output_dir log_vqgan --seed 0 --mixed_precision bf16 \
+    --model_type ctx_vqgan \
+    --train_batch_size 16 --gradient_accumulation_steps 1 --disc_start 1000005 \
+    --oxe_data_mixes_type bair --resolution 64 --dataloader_num_workers 16 \
+    --rand_select --video_stepsize 1 --segment_horizon 16 --segment_length 8 --context_length 1 \
+    --pretrained_model_name_or_path pretrained_models/ivideogpt-oxe-64-act-free/tokenizer
+
+
+accelerate launch train_gpt.py \
+    --exp_name bair_llama_ft --output_dir log_trm --seed 0 --mixed_precision bf16 \
+    --vqgan_type ctx_vqgan \
+    --pretrained_model_name_or_path {log directory of finetuned tokenizer}/unwrapped_model \
+    --config_name configs/llama/config.json \
+    --pretrained_transformer_path pretrained_models/ivideogpt-oxe-64-act-free/transformer \
+    --per_device_train_batch_size 16 --gradient_accumulation_steps 1 \
+    --learning_rate 1e-4 --lr_scheduler_type cosine \
+    --oxe_data_mixes_type bair --resolution 64 --dataloader_num_workers 16 \
+    --video_stepsize 1 --segment_length 16 --context_length 1 \
+    --use_eval_dataset --use_fvd --use_frame_metrics \
+    --weight_decay 0.01 --llama_attn_drop 0.1 --embed_no_wd
